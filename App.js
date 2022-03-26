f2fbb8405d38938c28b5a64808bffafd8aaba61a
KronosGR <geo.elgeo@gmail.com>
2022-03-26 06:47:40 +0100

    load custom fonts

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 
+import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
+import {
+  useFonts as useLato,
+  Lato_400Regular,
+  Lato_700Bold,
+} from '@expo-google-fonts/lato';
+
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurant.screen';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/infrastructure/theme/index';
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({ Oswald_400Regular });
+  const [latoLoaded] = useLato({ Lato_400Regular, Lato_700Bold });
+
+  if (!oswaldLoaded || !latoLoaded) return null;
+
   return (
     <>
       <ThemeProvider theme={theme}>
