74069e177ac668066d037e793ebd9407c76aca64
KronosGR <geo.elgeo@gmail.com>
2022-03-29 14:03:26 +0200

    added activity indicator

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Searchbar } from 'react-native-paper';
+import { Searchbar, ActivityIndicator, Colors } from 'react-native-paper';
 import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
 
@@ -7,6 +7,15 @@ import { RestaurantInfoCard } from '../components/restaurant-info-card.component
 import { Spacer } from '../../../components/spacer/spacer.component';
 import { SafeArea } from '../../../components/utility/safe-area.components';
 import { RestaurantsContext } from '../../../services/restaurants/restaurants.context';
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -20,8 +29,14 @@ const RestaurantList = styled(FlatList).attrs({
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
