abdb4b08af8ebde10d03b3f5c20f8367f267cce5
kronosGR <geo.elgeo@gmail.com>
2022-04-02 07:42:27 +0200

    setting the map region

--- src/features/map/screens/map.screens.js
+++ src/features/map/screens/map.screens.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import MapView from 'react-native-maps';
 import styled from 'styled-components';
+import { LocationContext } from '../../../services/location/location.context';
 import { Search } from '../components/search.component';
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context';
 
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
 
-export const MapScreen = () => (
-  <>
-    <Search />
-    <Map />
-  </>
-);
+export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    const latDelta = northeastLat - southwestLat;
+    setLatDelta(latDelta);
+  }, [location, viewport]);
+
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}>
+        {restaurants.map((restaurant) => null)}
+      </Map>
+    </>
+  );
+};
