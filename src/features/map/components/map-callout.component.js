a0740f873e23e78e7bd0caf11f949bb37a5c4a2c
kronosGR <geo.elgeo@gmail.com>
2022-04-02 12:20:41 +0200

    custom callout finished and clickable

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const MyText = styled.Text``;
+import { CompactRestaurantInfo } from '../../../components/restaurant/compact-restaurant-info.component';
 
 export const MapCallout = ({ restaurant }) => {
-  return <MyText>{restaurant.name}</MyText>;
+  return <CompactRestaurantInfo restaurant={restaurant} />;
 };
