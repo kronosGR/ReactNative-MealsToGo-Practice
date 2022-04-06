fa61905089c60d2e878fdd5b831d801d1b2f86fa
kronosGR <geo.elgeo@gmail.com>
2022-04-06 08:52:52 +0200

    aaded acount navigation

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { Text } from 'react-native';
@@ -32,16 +31,14 @@ const createScreenOptions = ({ route }) => {
 };
 
 export const AppNavigator = () => (
-  <NavigationContainer>
-    <Tab.Navigator
-      screenOptions={createScreenOptions}
-      tabBarOptions={{
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
-      }}>
-      <Tab.Screen name='Restaurants' component={RestaurantNavigator} />
-      <Tab.Screen name='Map' component={MapScreen} />
-      <Tab.Screen name='Settings' component={Settings} />
-    </Tab.Navigator>
-  </NavigationContainer>
+  <Tab.Navigator
+    screenOptions={createScreenOptions}
+    tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    }}>
+    <Tab.Screen name='Restaurants' component={RestaurantNavigator} />
+    <Tab.Screen name='Map' component={MapScreen} />
+    <Tab.Screen name='Settings' component={Settings} />
+  </Tab.Navigator>
 );
--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import React, { useContext } from 'react';
+
+import { AuthenticationContext } from '../../services/authentication/authentication.context';
+import { AccountNavigator } from './account.navigator';
 import { AppNavigator } from './app.navigator';
 
 export const Navigation = () => {
-  return <AppNavigator />;
+  const { isAuthenticated } = useContext(AuthenticationContext);
+
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <AppNavigator /> : <AccountNavigator />}
+    </NavigationContainer>
+  );
 };
